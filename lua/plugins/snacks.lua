fa89e20549e6d09124cab6eafe3248afee614f4f
Laus Bigum <laus@nordbo-robotics.com>
2025-10-30 12:26:22 +0100

    Configure snacks.picker to use a vertical split

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,7 +13,27 @@ return {
       enabled = true,
       timeout = 3000,
     },
+    --@type snacks.config.picker
     picker = {
+      layout = { preset = "vertical", },
+      layouts = {
+        vertical = {
+          layout = {
+            backdrop = false,
+            width = 0.85,
+            min_width = 80,
+            height = 0.9,
+            min_height = 30,
+            box = "vertical",
+            border = "rounded",
+            title = "{title} {live} {flags}",
+            title_pos = "center",
+            { win = "input", height = 1, border = "bottom" },
+            { win = "list", border = "none" },
+            { win = "preview", title = "{preview}", height = 0.6, border = "top" },
+          },
+        },
+      },
       enabled = true,
       sources = {
         explorer = {
