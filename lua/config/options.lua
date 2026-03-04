98806b346caf290d6bed6519b05797221e45a822
Laus Bigum <laus@nordbo-robotics.com>
2026-03-04 21:31:19 +0100

    Add fileformats

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,7 @@ vim.g.maplocalleader = " "                              -- change localleader to
 vim.g.loaded_netrw = 1                                  -- disable netrw
 vim.g.loaded_netrwPlugin = 1                            --  disable netrw
 
+vim.opt.fileformats = { "unix", "dos" }
 vim.opt.incsearch = true                                -- make search act like search in modern browsers
 vim.opt.backup = false                                  -- creates a backup file
 vim.opt.clipboard = "unnamedplus"                       -- allows neovim to access the system clipboard
