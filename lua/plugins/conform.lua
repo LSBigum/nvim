6393b53327e6daaf5c56f07937dd90d3d7ec58ab
Laus Bigum <laus@nordbo-robotics.com>
2026-04-24 11:59:34 +0200

    refactor(conform): Disable python linting for now

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -40,7 +40,7 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      python = { "isort", "black" },
+      -- python = { "isort", "black" },
       -- cpp = { "clang-format" },
       cpp = { "albatross_clang_format" },
       yaml = { "yamlfix" },
