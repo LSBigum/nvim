ad7aa39ed5f6022cf3fe032e7917475c101e3600
Laus Bigum <laus@nordbo-robotics.com>
2026-04-24 11:57:54 +0200

    feat(treesitter context): Toggle context with keybind

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -1,13 +1,18 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufNewFile" },
     config = function(_, opts)
       require("treesitter-context").setup(opts)
       -- Add a subtle underline “border” under the context window
       vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "Grey" })
       vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true, sp = "Grey" })
     end,
+    keys = {
+      { "<leader>ut", "<cmd>TSContext toggle<cr>", desc = "Toggle Treesitter Context" },
+    },
     opts = {
+      enable = true,
       max_lines = 0,
       trim_scope = "inner",
     },
