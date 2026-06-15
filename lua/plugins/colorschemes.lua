ffda18f0ba08b33ea2a3e842e4c14b4a62ea1ab2
Laus Bigum <laus@nordbo-robotics.com>
2026-06-15 22:19:24 +0200

    refactor(nvim): Set background to medium

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -367,7 +367,7 @@ return {
     config = function()
       vim.g.gruvbox_material_transparent_background = 0
       vim.g.gruvbox_material_foreground = "mix"
-      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_background = "medium"
       vim.g.gruvbox_material_ui_contrast = "high"
       vim.g.gruvbox_material_float_style = "bright"
       vim.g.gruvbox_material_statusline_style = "material"
